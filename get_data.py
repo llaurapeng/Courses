a291ed1ddd4f6ee2d8b02ceecaa004cc1322d911
“Laura <lp244@duke.edugit config --global user.name Laura Penggit config --global user.email lp244@duke.edu>
2024-09-02 18:39:11 -0400

    del

--- get_data.py
+++ get_data.py
@@ -46,7 +46,7 @@ class setup:
         '''
 
         options = webdriver.ChromeOptions()
-        options.add_argument('--headless')
+        #options.add_argument('--headless')
         options.add_argument('--disable-gpu')
         options.add_argument('--window-size=1920,1200')
         self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
